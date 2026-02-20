4ccd5d5eb8eed6ef55712535cecd29e39e468b8d
Sam <sam.bracamonte3@gmail.com>
2026-02-20 09:44:13 -0300

    Feat Muestra de colores seleccionados

--- colorcounter.js
+++ colorcounter.js
@@ -1,12 +1,17 @@
 
 let colorsArray = [];
+
 function generateColors() {
+    const n = document.getElementById("BoxQuantity").value;
+
     colorsArray = [];
     for (i = 0; i < 9; i++){
         colorsArray[i] = generateRandomColor();
     }
-    displayColors();
+    displayColors(n);
 }
+
+// Generador de colores HEX
 function generateRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -15,9 +20,16 @@ function generateRandomColor() {
     }
     return color;
 }
-function displayColors() {
-    for (i = 0; i < 9; i++){
-        document.getElementById("colorBox" + (i+1)).style.backgroundColor = colorsArray[i];}
-}
 
-const ColorQuantity = document.querySelectorAll("select")
+// Colores dependiendo la cantidad seleccionada
+function displayColors(n) {
+    for (i = 0; i < 9; i++){
+        const box = document.getElementById("colorBox" + (i+1));
+        if (i < n) {
+            box.style.backgroundColor = colorsArray[i];
+            box.style.display = "block";
+        } else {
+            box.style.display = "none";
+        }
+    }
+}
